09e9db8b66fdcee9b228c8b16ed8cce9d2a39f54
kreeos <kanghyun.kris.lee@gmail.com>
2021-04-11 15:17:04 +0900

    First Release Complete

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,166 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Kris Lee</title>
+        <link rel="icon" href="/icon-person.png" />
       </Head>
-
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className="page">
+          <h1 className="title">
+            <a>Hello, World!</a>
+          </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          <img className="image-portrait"/>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <p className="description">
+            My name is KangHyun Lee, and you can call me by Kris!<br/>
+            I just graduated from college, and just getting ready to spread my wings.
+            <br/>
+            Let me show you couple things about me.
+          </p>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <div className="page">
+            <div className="links">
+              <a href="mailto:kanghyunkrislee@gmail.com" target="blank" className="container-link">
+                <img src="/email-logo.png" alt="Email Logo" className="logo-email"/>
+                {' '}E-mail 
+              </a>
+              <a href="https://instrgram.com/kreeos47" target="blank" className="container-link">
+                <img src="/insta-logo.png" alt="Insta Logo" className="logo-contact"/>
+                {' '}Instagram 
+              </a>
+              <a href="https://github.com/kreeos" target="blank" className="container-link">
+                <img src="/github.svg" alt="Github Logo" className="logo-contact"/>
+                {' '}Github 
+              </a>
+              <a href="KangHyun_Kris_Lee_CV_20210411.pdf" target="blank" className="container-link">
+                <img src="/resume.svg" alt="Resume Logo" className="logo-contact"/>
+                {' '}Resume 
+              </a>
+            </div>
+          </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <hr></hr>
         </div>
+
+
+      	<div className="page">
+      	  <a className="section-title">
+      	    Projects
+      	  </a>
+      	  <div className="grid">
+            <a href="https://pioneers.kaist.ac.kr" className="card">
+              <h3>PIONEERS 2071 &rarr;</h3>
+              <p>
+                KAIST 50th year anniversary website. 
+                <br/>I created this website using <code>React.js</code>,<code>MongoDB</code> and <code>Node.js</code>.
+      	        <br/>
+      	        <a className="link" href="https://github.com/kreeos/pioneers2071"> 
+                  Link to Repo 
+                </a>
+      	      </p>
+            </a>
+            <a href="https://rnl.kaist.ac.kr" className="card">
+              <h3>RNL Website &rarr;</h3>
+              <p>
+                Radiation Materials and Nanomechanics lab website. 
+                <br/>I created this website using <code>React.js</code>, <code>MongoDB</code> and <code>Node.js</code>
+        	  	  <br/> 
+                <a className="repo">
+                  The project repository is <a className="private">private</a>
+                </a>.
+      	      </p>
+            </a>
+            <a href="https://horoscope.krislee.dev" className="card">
+              <h3>Daily Horoscope &rarr;</h3>
+              <p>
+                A website for your daily horoscope. 
+                <br/>I created this website using <code>HTML</code>, <code>CSS</code>, <code>MongoDB</code> and <code>Node.js</code>
+      	        <br/>
+                <a className="link" href="https://github.com/kreeos/newbie_project"> 
+                  Link to Repo 
+                </a>
+      	      </p>
+            </a>
+            <a
+              href="https://github.com/ANLAB-KAIST/KENSv3" className="card"
+            >
+              <h3>KENS &rarr;</h3>
+              <p>
+                  KAIST Educational Network System <br/>
+      		        The system mainly consists of <code>C++</code>, and the main purpose is to build <code>TCP/IP</code>
+      	  	      <br/> 
+                  <a className="repo">
+                    The project repository is <a className="private">private</a>
+                  </a>.
+      	      </p>
+      	    </a>
+            <a
+              href="https://www.scs.stanford.edu/17wi-cs140/pintos/pintos.html" className="card"
+            >
+              <h3>PintOS &rarr;</h3>
+              <p>
+                Originally designed for Standford CS140, modified for KAIST CS330<br/>
+            		The system consists of 100% <code>C</code> Language. <br/>
+            		Threading, scheduling, syscall, VM, and file system were implemented.
+      	  	    <br/> 
+                <a className="repo">
+                  The project repository is <a className="private">private</a>
+                </a>.
+      	      </p>
+            </a>
+      	  </div>
+      	  <hr></hr>
+        </div>
+
+
+      	<div className="page">
+      	  <a className="section-title">
+      	    Experiences 
+      	  </a>
+      	  <div className="grid">
+            <a className="exp-card">
+              <h3>LG CNS</h3>
+              <p>
+      		      <a className="date"> 2021.01 - Current </a><br/>
+      	        <p> 
+                  Working as an Infra Architect 
+                </p>
+      	      </p>
+      	    </a>
+            <a className="exp-card">
+              <h3>LG CNS - Intern</h3> 
+              <p>
+      		      <a className="date"> 2020.01 - 2020.02 </a><br/>
+      	        <p> 
+                  Internship as a Software Architect. 
+                </p>
+      	      </p>
+      	    </a>
+            <a className="exp-card">
+              <h3>Batoners - Intern </h3> 
+              <p>
+                <a className="date"> 2019.06 - 2019.08 </a><br/>
+                <p>
+                  Developing an Android application for visually handicapped people. 
+                </p>
+              </p>
+            </a>
+      	  </div>
+      	  <hr></hr>
+        </div>
+
+
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+        This website was created with {' '}
+        <img src="/nextjs-logo.png" alt="NextJs Logo" className="logo" />
       </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
